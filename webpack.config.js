eac9e85faa263f6df67bd686a8cda65d8fc5d254
d0wnsider <jmhandsamson@gmail.com>
2023-04-24 18:24:09 -0400

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: "index.html", // Specify the output HTML file name
+      inject: "body", // Specify where the script tag should be injected in the HTML file
+      scriptFilename: (chunk) =>
+        `${chunk.name !== "main" ? chunk.name + "/" : ""}[name].bundle.js`, // Use the correct bundle file name
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
