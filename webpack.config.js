a503c3f0c6bdfec3214541ecc341d411ca37ad2b
d0wnsider <jmhandsamson@gmail.com>
2023-04-24 20:19:37 -0400

    Update webpack settings and install normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,6 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -16,15 +13,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/template.html"),
-      filename: "index.html", // Specify the output HTML file name
-      inject: "body", // Specify where the script tag should be injected in the HTML file
-      scriptFilename: (chunk) =>
-        `${chunk.name !== "main" ? chunk.name + "/" : ""}[name].bundle.js`, // Use the correct bundle file name
     }),
   ],
-  optimization: {
-    runtimeChunk: "single",
-  },
+
   module: {
     rules: [
       {
