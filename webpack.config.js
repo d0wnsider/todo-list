f94ad957a1460b1154c47c9dd5fb34b1f1574c81
d0wnsider <jmhandsamson@gmail.com>
2023-04-26 15:21:08 -0400

    Update html and css

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,8 @@ module.exports = {
   entry: "./src/index.js",
   devtool: "inline-source-map",
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
